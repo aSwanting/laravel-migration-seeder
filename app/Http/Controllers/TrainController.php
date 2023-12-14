a3cfc59614ff32cbdb47c6bddc895e654914c2ee
aSwanting <gab.damico@gmail.com>
2023-12-14 10:32:18 +0100

    add: incorrect date (from not on)

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,8 +9,8 @@ class TrainController extends Controller
 {
     public function fetchTrains()
     {
-        // $trains = Train::where('departure_date', date('Y-m-d'))->get();
-        $trains = Train::wheredate('departure_date', today())->get();
+        // $trains = Train::where('departure_date', '>=', date('Y-m-d'))->get();
+        $trains = Train::wheredate('departure_date', '>=', today())->get();
         return view('home', compact('trains'));
     }
 }
